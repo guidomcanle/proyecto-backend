04ed99061f59a68d5802026f96318d35b97d2c58
Guido Matías Canle <guidocanle@gmail.com>
2022-03-03 20:18:38 -0300

    Arreglando el desafío

--- server.js
+++ server.js
@@ -29,9 +29,7 @@ app.get("/ProductoRandom", async (requerido, respuesta) => {
 const PORT = 8080;
 
 const server = app.listen(PORT, () => {
-  console.log(
-    `Servidor http escuchando en el puerto ${server.address().port}😎`
-  );
+  console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
 });
 
 server.on("error", (error) => console.log(`Error en servidor ${error}`));
