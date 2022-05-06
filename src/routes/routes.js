d90ecf7b88197bfa5402504df89166eef5e57d32
Guido Matías Canle <guidocanle@gmail.com>
2022-05-06 14:35:53 -0300

    actualización respuestas.json y algunos if

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -30,19 +30,22 @@ router
     respuesta.redirect("/api/productos");
   })
   .delete(async (requerido, respuesta) =>
-    respuesta.send(await contenedor.deleteAll())
+    respuesta.json({ eliminado: await contenedor.deleteAll() })
   );
 
 router
   .route("/productos/:id")
   .get(async (requerido, respuesta) => {
     const id = requerido.params.id;
+    console.log(id);
 
     // No funciona este if, arreglar
-    if (id) {
-      respuesta.send(await contenedor.getById(id));
+    const prod = await contenedor.getById(requerido.params.id);
+    console.log(prod);
+    if (prod != null) {
+      respuesta.json({ producto: await contenedor.getById(id) });
     } else {
-      respuesta.send({ error: "El producto no existe" });
+      respuesta.json({ error: "El producto no existe" });
     }
   })
   .put(async (requerido, respuesta) => {
@@ -52,67 +55,90 @@ router
     const thumbnail = String(requerido.body.thumbnail);
     const description = String(requerido.body.description);
     const stock = Number(requerido.body.stock);
-    await contenedor.update(id, {
-      title,
-      description,
-      thumbnail,
-      price,
-      stock,
+    respuesta.json({
+      producto: await contenedor.update(id, {
+        title,
+        description,
+        thumbnail,
+        price,
+        stock,
+      }),
     });
-    respuesta.send(contenedor);
   })
   .delete(async (requerido, respuesta) => {
     const id = requerido.params.id;
-    const arrayNuevo = await contenedor.deleteById(id);
-
-    respuesta.send({ nuevoArray: arrayNuevo });
+    respuesta.json({ nuevoArray: await contenedor.deleteById(id) });
   });
 
 router
   .route("/carrito")
   .get(async (requerido, respuesta) => {
-    respuesta.send(await contenedorCarrito.getAll());
+    respuesta.json({ carritos: await contenedorCarrito.getAll() });
   })
   .post(async (requerido, respuesta) => {
-    respuesta.send(await contenedorCarrito.save());
+    respuesta.json({ carritoCreado: await contenedorCarrito.save() });
   })
   .delete(async (requerido, respuesta) => {
-    respuesta.send(await contenedorCarrito.deleteAll());
+    respuesta.json({ borrado: await contenedorCarrito.deleteAll() });
   });
 
 router
   .route("/carrito/:id")
   .get(async (requerido, respuesta) => {
     const id = requerido.params.id;
-
-    respuesta.send(await contenedorCarrito.getById(id));
+    console.log(await contenedorCarrito.getById(id));
+    if (await contenedorCarrito.getById(id)) {
+      respuesta.json({ carrito: await contenedorCarrito.getById(id) });
+    } else {
+      respuesta.json({
+        error: "El carrito no existe",
+      });
+    }
   })
   .delete(async (requerido, respuesta) => {
     const id = requerido.params.id;
+    const cart = await contenedorCarrito.getById(requerido.params.id);
     const arrayNuevo = await contenedorCarrito.deleteById(id);
-
-    respuesta.send({ nuevoArray: arrayNuevo });
+    console.log(cart);
+    console.log(arrayNuevo);
+    if (cart) {
+      respuesta.json({ carritoBorrado: arrayNuevo });
+    } else {
+      respuesta.json({
+        error: "El carrito no existe",
+      });
+    }
   });
 
 router
   .route("/carrito/:id/productos")
   .get(async (requerido, respuesta) => {
     const id = requerido.params.id;
-    respuesta.send(await contenedorCarrito.getById(id));
+    console.log(await contenedorCarrito.getById(id));
+    if (id) {
+      respuesta.json({
+        productosEnElCarrito: await contenedorCarrito.getById(id),
+      });
+    } else {
+      respuesta.json({
+        error: "El carrito no existe",
+      });
+    }
   })
   .post(async (requerido, respuesta) => {
     const cart = await contenedorCarrito.getById(requerido.params.id);
     const prod = await contenedor.getById(requerido.body.idProd);
-
+    // No funciona el if
     if (
       prod._id.valueOf() === requerido.body.idProd &&
       cart._id.valueOf() === requerido.params.id
     ) {
       await cart.productos.push(prod);
-      await contenedorCarrito.update(cart._id, cart);
-      respuesta.send("Actualizado el carrito");
+      respuesta.json({
+        productoAgregado: await contenedorCarrito.update(cart._id, cart),
+      });
     } else {
-      respuesta.send("No se pudo actualizar el carrito");
+      respuesta.json("No se pudo actualizar el carrito");
     }
   })
   .delete(async (requerido, respuesta) => {
@@ -120,12 +146,13 @@ router
     const prodIndex = cart.productos.findIndex(
       (p) => p._id.valueOf() === requerido.body.idProd
     );
-
     if (prodIndex != -1) {
       cart.productos.splice(prodIndex, 1);
-      respuesta.send(await contenedorCarrito.update(cart._id, cart));
+      respuesta.json({
+        nuevoArray: await contenedorCarrito.update(cart._id, cart),
+      });
     } else {
-      respuesta.send({
+      respuesta.json({
         error: "El carrito no existe o el producto no está en el carrito",
       });
     }
